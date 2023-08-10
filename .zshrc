3875349bfe92215ba09c96ae000004a7866ff586
rohitpid <rohitpid@gmail.com>
2023-08-10 10:26:56 -0700

    feat: update to flatpaks and update repos for 2023 (#3)
    * Move to flatpaks for some common programs
    * Update PPA and keys
    * Update all submodules
    * Move to powerlevel10k

--- .zshrc
+++ .zshrc
@@ -6,7 +6,7 @@ export ZSH=~/.oh-my-zsh
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
 #ZSH_THEME="rohit"
-ZSH_THEME="powerlevel9k/powerlevel9k"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
--- install.sh
+++ install.sh
@@ -10,17 +10,16 @@ if  [[ $(uname -s) == "Linux" ]] && [[ $(lsb_release -i | awk '{print $3}') == "
     echo -e "${GREEN}Detected Ubuntu. Installing PPAs and packages from UbuntuPackages file${NC}"
     printf '#%.0s' {1..80}
     printf '\n'
-    sudo apt-get install curl
-    ./UbuntuPPA
+    ./ubuntu_ppas
     sudo apt-get update
-    xargs -a <(awk '/^\s*[^#]/' "UbuntuPackages") -r -- sudo apt-get install -y
+    xargs -a <(awk '/^\s*[^#]/' "ubuntu_packages") -r -- sudo apt-get install -y
     printf '#%.0s' {1..80}
     printf '\n'
-    echo -e "${GREEN}Installing snaps for slack and clion${NC}"
+    echo -e "${GREEN}Installing flatpaks${NC}"
+    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    xargs -a <(awk '/^\s*[^#]/' "flatpaks") -r -- sudo flatpak -y install
     printf '#%.0s' {1..80}
     printf '\n'
-    sudo snap install slack --classic
-    sudo snap install clion --classic
 fi
 
 #if [[ $(uname -s) == "Linux" ]] && $(echo $SHELL) != "/bin/zsh" ]]; then
@@ -45,13 +44,13 @@ printf '\n'
 sudo chsh -s /bin/zsh $USER
 cp .zshrc ~/.zshrc
 
-printf '#%.0s' {1..80}
-printf '\n'
-echo -e "${GREEN}Setting up vim plugins and vimrc${NC}"
-printf '#%.0s' {1..80}
-printf '\n'
-cp .vimrc ~/.vimrc
-cp -a .vim ~/.vim
+# printf '#%.0s' {1..80}
+# printf '\n'
+# echo -e "${GREEN}Setting up vim plugins and vimrc${NC}"
+# printf '#%.0s' {1..80}
+# printf '\n'
+# cp .vimrc ~/.vimrc
+# cp -a .vim ~/.vim
 
 if [ $USER == "rohit" ];
 then
@@ -72,12 +71,12 @@ cp -a bin ~/
 
 printf '#%.0s' {1..80}
 printf '\n'
-echo -e "${GREEN}Setting up oh-my-zsh theme and powerlevel9k${NC}"
+echo -e "${GREEN}Setting up oh-my-zsh theme and powerlevel10k${NC}"
 printf '#%.0s' {1..80}
 printf '\n'
 cp -a .oh-my-zsh ~/.oh-my-zsh
 cp rohit.zsh-theme ~/.oh-my-zsh/themes/
-cp -a powerlevel9k ~/.oh-my-zsh/themes/
+cp -a powerlevel10k ~/.oh-my-zsh/themes/
 cp .zsh_profile_remote ~/.zsh_profile_remote
 cp .zshrc_mac ~/.zshrc_mac
 
@@ -87,4 +86,3 @@ echo -e "${GREEN}Setting up tmux.conf  then starting tmux${NC}"
 printf '#%.0s' {1..80}
 printf '\n'
 cp .tmux.conf ~/.tmux.conf
-tmux attach -t main || tmux new -s main
